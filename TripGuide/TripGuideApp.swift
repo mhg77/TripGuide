7c0834444815c608c4be91878826fd6a52bda1c9
mhg77 <hashier@yandex.ru>
2026-07-17 20:57:56 +0300

    Initial Commit

---
+++ TripGuide/TripGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripGuideApp.swift
+//  TripGuide
+//
+//  Created by Михаил Асаилов on 17.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct TripGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
